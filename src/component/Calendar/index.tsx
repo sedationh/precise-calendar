688f8299f238dc70cab6093d17de9321c3e4198b
SedationH <sedationh@gmail.com>
2025-03-29 10:02:07 +0800

    ✨(calendar): Add Bootstrap 5 styling to FullCalendar

--- src/component/Calendar/index.tsx
+++ src/component/Calendar/index.tsx
@@ -1,13 +1,17 @@
-import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import bootstrap5Plugin from '@fullcalendar/bootstrap5'
+import dayGridPlugin from '@fullcalendar/daygrid'
 import FullCalendar from '@fullcalendar/react'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 export default function Calendar() {
   return (
     <FullCalendar
-      plugins={[dayGridPlugin]}
+      plugins={[dayGridPlugin, bootstrap5Plugin]}
       buttonText={{
         today: 'Today',
       }}
+      themeSystem="bootstrap5"
     />
   )
 }
