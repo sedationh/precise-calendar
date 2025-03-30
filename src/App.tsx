63ca6ea6c6a88e7d866f375be355994cac59c5a0
SedationH <sedationh@gmail.com>
2025-03-30 14:07:49 +0800

    feat: add @vercel/analytics package and integrate analytics component into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Analytics } from '@vercel/analytics/react'
 import Calendar from './component/Calendar'
 
 function App() {
   return (
     <div className="mx-auto max-w-screen-lg p-4">
       <Calendar />
+      <Analytics />
     </div>
   )
 }
